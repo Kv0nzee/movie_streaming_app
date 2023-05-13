463e4f1002e4c6384e8f0e3e9475a1a05120a669
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2023-05-13 14:47:28 +0630

    nextauth, prisma, mogo, google, github setup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,13 @@
+import axios from 'axios';
 import { useCallback, useState} from 'react';
 import Image from 'next/image'
+import { signIn } from 'next-auth/react';
 
 import Input from '@/components/Input';
+import router from 'next/router';
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
 
@@ -16,15 +21,46 @@ const Auth = () => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
   }, []);
 
+
+  const login = useCallback(async () => {
+    try{
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+
+      router.push('/');
+    } catch(error){
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async() => {
+    try{
+      await axios.post('/api/register',{
+        email,
+        name,
+        password
+      });
+
+      login();
+
+    }catch(error){
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
     return (
       <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-        <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <div className="w-full h-full bg-black lg:bg-opacity-50">
           <nav className="px-12 py-5">
             <Image src="/images/logo.png" className="h-12" alt="Logo"  width={800} height={500}/>
           </nav>
           <div className="flex justify-center">
-            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-              <h2 className="text-white text-4xl mb-8 font-semibold">
+            <div className="self-center w-full px-16 py-16 mt-2 bg-black rounded-md bg-opacity-70 lg:w-2/5 lg:max-w-md">
+              <h2 className="mb-8 text-4xl font-semibold text-white">
                 {variant === 'login' ? 'Sign in' : 'Register'}
               </h2>
               <div className="flex flex-col gap-4">
@@ -52,20 +88,20 @@ const Auth = () => {
                   onChange={(e: any) => setPassword(e.target.value)} 
                 />
               </div>
-              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              <button onClick={ variant === 'login' ? login : register } className="w-full py-3 mt-10 text-white transition bg-red-600 rounded-md hover:bg-red-700">
                 {variant === 'login' ? 'Login' : 'Sign up'}
               </button>
-              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                <div onClick={() => {}} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                  {/* <FcGoogle size={32} /> */}
+              <div className="flex flex-row items-center justify-center gap-4 mt-8">
+                <div onClick={() => signIn('google', { callbackUrl: '/' })} className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80">
+                  <FcGoogle size={32} />
                 </div>
-                <div onClick={() => {}} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                  {/* <FaGithub size={32} /> */}
+                <div onClick={() => signIn('github', { callbackUrl: '/' })} className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80">
+                  <FaGithub size={32} />
                 </div>
               </div>
-              <p className="text-neutral-500 mt-12">
+              <p className="mt-12 text-neutral-500">
                 {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-                <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                <span onClick={toggleVariant} className="ml-1 text-white cursor-pointer hover:underline">
                   {variant === 'login' ? 'Create an account' : 'Login'}
                 </span>
                 .
